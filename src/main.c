5b4625f5dfd162f8469f18ac8df4d739fc08b144
Melody Cherry <melody.laffitau@gmail.com>
2025-02-08 17:48:26 +0100

    tentative extract to stack qui marche pas

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mlaffita <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:05:17 by mlaffita          #+#    #+#             */
-/*   Updated: 2025/02/07 16:50:02 by mlaffita         ###   ########.fr       */
+/*   Updated: 2025/02/08 17:45:40 by mlaffita         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 int	parse_arg(char *p_arg, int *count);
 void	ft_error(char *s, int fd);
-int extract_to_stack(char *p_arg, int *stack, int *count);
+void extract_to_stack(char *p_arg, int **stack,  int *count);
 
 int	main (int argc, char *argv[])
 {
@@ -32,17 +32,26 @@ int	main (int argc, char *argv[])
 	}
 	count = 0;
 	i = 1;
+	stack_a = 0;
 	while (i < argc)
 	{
 		p_arg = argv[i];
 		i++;
-		//printf("main: %s\n", p_arg);
 		if (parse_arg(p_arg, &count) == 1)
-			return (1); // indique une erreur 
+			return (1);
 		else
+		{
 			extract_to_stack(p_arg, &stack_a, &count);
+		}
 	}
-	//printf("count :%d\n", count);
+	i = 0;
+	while (i < count) // test pour print la stack 
+	{
+		printf("%d\n", stack_a[i]);
+		i++;
+	}
+	free(stack_a);
+	printf("count :%d\n", count);
 	return (0);
 }
 
@@ -65,15 +74,24 @@ int	parse_arg(char *p_arg, int *count)
 		while (ft_isdigit(p_arg[i]))
 			i++;
 		(*count)++;
-		printf("parse: %s\n", p_arg);
 	}
 	printf("count :%d\n", *count);
 	return (0);
 }
 
-int extract_to_stack(char *p_arg, int *stack, int *count)
+void extract_to_stack(char *p_arg, int **stack_a, int *count)
 {
+	int i;
 	
+	i = 0;
+	*stack_a= malloc(sizeof(int) * (*count));
+	if (!*stack_a)
+		return;
+	while (p_arg[i])   // pas correct 
+	{
+		(*stack_a)[i] = atoi(&p_arg[i]);
+		i++;
+	}
 }
 
 // a mettre dans utils ? 
